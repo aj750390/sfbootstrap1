77c4a606057fe41d8a2ff215ba3a8239828fdb0e
aj750390 <53556291+aj750390@users.noreply.github.com>
2026-07-01 03:26:47 +0530

    Create post-build-mw.sh for Xiaomi Mi A1
    Added a post-build script for Xiaomi Mi A1 to handle middleware tasks.

--- device/xiaomi-tissot/post-build-mw.sh
+++ device/xiaomi-tissot/post-build-mw.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# This script is executed after building the middleware.
+# It can be used to apply device-specific patches or perform other setup tasks.
+
+echo "Running post-build-mw.sh for Xiaomi Mi A1 (tissot)"
+
+# No specific post-build middleware steps identified for tissot yet, keeping generic.
